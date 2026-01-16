3f1058fa14afe3a29e5089caebbfe5992b732e54
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-01-16 10:24:48 -0700

    Beginning BlackJack capstone project.

---
+++ Day 11/main.py
@@ -0,0 +1,17 @@
+from art import logo
+import random
+import os
+# Our Blackjack Game House Rules
+# The deck is unlimited in size.
+# There are no jokers.
+# The Jack/Queen/King all count as 10.
+# The Ace can count as 11 or 1.
+# Use the following list as the deck of cards:
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# The cards in the list have equal probability of being drawn.
+# Cards are not removed from the deck as they are drawn.
+# The computer is the dealer.
+wants_to_play = input("Do you want to play a game of BlackJack? (y/n): ").lower
+if wants_to_play == "y":
+    print(logo)
