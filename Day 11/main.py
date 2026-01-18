332b1383038902e2fe7571dacdcc5368ebef4ff3
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-01-18 13:51:37 -0700

    Added function definitions. Optimized times score calculating runs.

--- Day 11/main.py
+++ Day 11/main.py
@@ -15,36 +15,38 @@ player_hand = []
 dealer_hand = []
 
 def calculate_totals():
+    """Calculates both hands of cards. Returns the totals of each hand."""
     player_total = sum(player_hand)
     dealer_total = sum(dealer_hand)
     return player_total, dealer_total
 
 def initial_deal():
+    """Deals two cards and shows the starting hands to user. Returns the totals of each hand."""
     for hand in (player_hand, dealer_hand):
         hand.clear()
-    for card in range(0,2):
+    for card in range(2):
         dealer_hand.append(random.choice(cards))
         player_hand.append(random.choice(cards))
-        player_total, dealer_total = calculate_totals()
+    player_total, dealer_total = calculate_totals()
     print(f"Dealer's hand: [{dealer_hand[0]}, hidden]")
     print(f"Player's hand: {player_hand} = {player_total}")
     return player_total, dealer_total
 
 def change_ace(player_total, dealer_total):
-    if 11 in player_hand and player_total > 21:
-        while player_total > 21:
-            ace = player_hand.index(11)
-            player_hand[ace] = 1
-            player_total, dealer_total = calculate_totals()
+    """If over 21 and has an Ace, changes the Ace to a 1. Returns the totals of each hand."""
+    while 11 in player_hand and player_total > 21:
+        ace = player_hand.index(11)
+        player_hand[ace] = 1
+        player_total, dealer_total = calculate_totals()
             
-    if 11 in dealer_hand and dealer_total > 21:
-        while dealer_total > 21:
-            ace = dealer_hand.index(11)
-            dealer_hand[ace] = 1
-            player_total, dealer_total = calculate_totals()
+    while 11 in dealer_hand and dealer_total > 21:
+        ace = dealer_hand.index(11)
+        dealer_hand[ace] = 1
+        player_total, dealer_total = calculate_totals()
     return calculate_totals()
 
 def player_hit(player_total):
+    """Deals a card to the player. Shows their hand at the end, and returns the player_total"""
     if player_total == 21:
         print("You are at 21.")
     else:
@@ -52,15 +54,19 @@ def player_hit(player_total):
         player_total, dealer_total = calculate_totals()
         player_total, dealer_total = change_ace(player_total, dealer_total)      
     print(f"Player's hand: {player_hand} = {player_total}")
+    return player_total
 
    
 def dealer_hit(dealer_total):
+    """Deals a card to the dealer if under 17, changes the Ace if they've gone over, and returns the dealer_total"""
     while dealer_total < 17:
         dealer_hand.append(random.choice(cards))
         player_total, dealer_total = calculate_totals()
+        change_ace(player_total, dealer_total)
     return dealer_total
     
 def calculate_winner(player_total, dealer_total):
+    """Game logic for who wins."""
     if player_total == dealer_total:
         print(f"Dealer's current hand is: {dealer_hand} = {dealer_total}")
         print(f"Player's current hand is: {player_hand} = {player_total}")
@@ -78,6 +84,7 @@ keep_playing = True
 while keep_playing:
     wants_to_play = input("Do you want to play a game of BlackJack? (y/n): ").lower()  
     if wants_to_play == "y":
+        os.system('cls' if os.name == "nt" else 'clear')
         print(logo)
         player_total, dealer_total = initial_deal()
         if player_total == 22 or dealer_total == 22:
@@ -98,8 +105,12 @@ while keep_playing:
                 print("Your input was invalid. Type 'hit' or 'stay'... ")       
         if player_total > 21: 
             print("You went over. Dealer wins automatically.")   
+            print(f"Dealer's current hand is: {dealer_hand} = {dealer_total}")
+            print(f"Player's current hand is: {player_hand} = {player_total}")
         elif dealer_total > 21:  
             print("Dealer went over. Player wins!")
+            print(f"Dealer's current hand is: {dealer_hand} = {dealer_total}")
+            print(f"Player's current hand is: {player_hand} = {player_total}")
         else:
             calculate_winner(player_total, dealer_total)
     else:
