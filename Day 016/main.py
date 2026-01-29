8241445328b7989cb64d24cd79f0bf5067cf7575
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-01-28 20:23:57 -0700

    Need to add money function

--- Day 016/main.py
+++ Day 016/main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+CoffeeMaker = CoffeeMaker()
+Menu = Menu()
+Money = MoneyMachine()
+
+machine_on = True
+while machine_on:
+    user_selection = input("Would you like to view 'resources', 'order' a drink, or turn the machine 'off'? ")
+    if user_selection == "off":
+        machine_on = False
+    elif user_selection == "resources":
+        print(CoffeeMaker.report())
+    elif user_selection == "order":
+        order = input(f"What would you like to order: {Menu.get_items()} ? \n")
+        drink = Menu.find_drink(order)
+        if CoffeeMaker.is_resource_sufficient(drink):
+                CoffeeMaker.make_coffee(drink)
+        
+    else:
+        print("That was not a valid selection.")
