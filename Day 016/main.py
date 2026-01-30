1d1c5204705dd9d28c675c0d8d39e0b381b2f5fa
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-01-29 20:05:06 -0700

    Added payment and completed project for OOP

--- Day 016/main.py
+++ Day 016/main.py
@@ -2,9 +2,9 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-CoffeeMaker = CoffeeMaker()
-Menu = Menu()
-Money = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
 
 machine_on = True
 while machine_on:
@@ -12,12 +12,15 @@ while machine_on:
     if user_selection == "off":
         machine_on = False
     elif user_selection == "resources":
-        print(CoffeeMaker.report())
+        coffee_maker.report()
+        money_machine.report()
     elif user_selection == "order":
-        order = input(f"What would you like to order: {Menu.get_items()} ? \n")
-        drink = Menu.find_drink(order)
-        if CoffeeMaker.is_resource_sufficient(drink):
-                CoffeeMaker.make_coffee(drink)
-        
+        order = input(f"What would you like to order: {menu.get_items()} ? \n")
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)        
     else:
         print("That was not a valid selection.")
+        
+        
