082cd10b238283d0a408a571bdb3b2782724a969
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-01-31 12:18:09 -0700

    Project completed!

--- Day 017/main.py
+++ Day 017/main.py
@@ -1,5 +1,17 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
-for item in question_data:
-    print(item)
+question_bank = []
+for dictionary in question_data:
+    text = dictionary["text"]
+    answer = dictionary["answer"]
+    question = Question(text, answer)
+    question_bank.append(question)
+    
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+   
+print("You've completed the quiz. Thanks for playing!")
+print(f"Your high score was {quiz.score}/{quiz.question_number}")
