d93fcb545b332b453d4a18c042b7a27f3da1c51e
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-09 19:47:47 -0700

    forward, backward, left, right, and clear tied to screen event listener.

---
+++ Day 019/etch_a_sketch.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+turk = Turtle()
+turk.pendown()
+turk.pensize(5)
+screen = Screen()  
+
+def move_forward():
+    turk.forward(10)
+    
+def turn_right():
+    turk.right(15)
+    
+def turn_left():
+    turk.left(15)
+    
+def move_backwards():
+    turk.backward(10)
+
+def clear():
+    turk.clear()
+    turk.penup()
+    turk.home()
+    turk.pendown()
+
+screen.listen()
+
+
+screen.onkey(fun=move_forward, key="w")
+screen.onkey(fun=turn_right, key="d")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=clear, key="c")
+
+
+screen.exitonclick()
