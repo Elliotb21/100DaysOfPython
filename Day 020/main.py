282625368f5959d3b7a536ecd3cdeb10c049c169
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-13 14:36:29 -0700

    Snake object instantiated. Snake class created with attributes. Snake method move created.

---
+++ Day 020/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake!")
+screen.tracer(0)
+
+snake = Snake()
+
+
+game_over = False
+while not game_over:
+    screen.update()
+    time.sleep(0.1)     
+      
+    snake.move()
+
+
+screen.exitonclick()
---
+++ Day 020/snake.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+import time
+
+class Snake:
+    MOVE_DISTANCE = 20
+    def __init__(self):
+        STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+        self.snake_body = []
+        
+        for position in STARTING_POSITIONS:
+            segment = Turtle("square")
+            segment.penup()
+            segment.color("white")
+            segment.goto(position)
+            self.snake_body.append(segment)
+
+    def move(self):
+        for segment in range(len(self.snake_body) -1 , 0, -1):
+            new_x = self.snake_body[segment -1].xcor()
+            new_y = self.snake_body[segment -1].ycor()
+            self.snake_body[segment].goto(new_x, new_y)
+        self.snake_body[0].forward(self.MOVE_DISTANCE)    
+        self.snake_body[0].left(90)
