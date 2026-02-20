12c47d81d49ce6a9ef485640f12029a4da69f703
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-20 16:57:22 -0700

    Car manager creates car objects randomly on y-axis. Move method moves the cars across the x-axis only. Next_level method increases class speed attribute

--- Day 023/car_manager.py
+++ Day 023/car_manager.py
@@ -1,7 +1,33 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle
+from random import choice, randint
+
+COLORS = ["red", "orange", "magenta", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    car_speed = STARTING_MOVE_DISTANCE
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_len=2.5, stretch_wid= 1, outline=1)
+        self.color(choice(COLORS))
+        self.goto(300, self.starting_y())
+   
+   
+    def starting_y(self):
+        return randint(-250, 250)
+        
+        
+    def move(self):
+        current_x = self.xcor()
+        current_y = self.ycor()
+        new_x = current_x - self.car_speed
+        self.goto(new_x, current_y)
+        
+        
+    def next_level(self):
+        CarManager.car_speed += MOVE_INCREMENT
