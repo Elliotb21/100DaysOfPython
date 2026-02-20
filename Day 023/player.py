e9fb18f85aa2016c8394580ea189b02749c36ada
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-20 16:58:19 -0700

    player creates a turtle object. Move method moves only up on y-axis. Safely_crossed returns if player makes it to top edge of screen.

--- Day 023/player.py
+++ Day 023/player.py
@@ -1,7 +1,32 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.setheading(90)
+        self.penup()
+        self.color("black")
+        self.shape("turtle")
+        self.goto(STARTING_POSITION)
+        
+    
+    def move(self):
+        current_x = self.xcor()
+        current_y = self.ycor()
+        new_y = current_y + MOVE_DISTANCE
+        self.goto(current_x, new_y)
+
+
+    def safely_crossed(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        
+        
+    def reset(self):
+        self.goto(STARTING_POSITION)
