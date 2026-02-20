f5b8470ea21b08192d5552e99462c9a35d5a9bf3
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-20 16:59:01 -0700

    Scoreboard object creation. Update_score method and game_over method do just what they say.

--- Day 023/scoreboard.py
+++ Day 023/scoreboard.py
@@ -1,5 +1,30 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.current_score = 0
+        self.penup()
+        self.hideturtle()
+        self.color("black")
+        self.goto(-280, 260)
+        self.write_score()
+        
+        
+    def write_score(self):
+        self.write(f"Level: {self.current_score}", font=FONT)
+        
+        
+    def update_score(self):
+        self.clear()
+        self.current_score += 1
+        self.write_score()
+        
+    
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Ouch!\n\n ...Game Over...", align="left", font=FONT)
+
