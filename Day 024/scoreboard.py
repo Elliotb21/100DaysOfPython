f229d0b43e2da70340765c1fffe0417252f0dad6
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-21 19:26:05 -0700

    Read and write to snake_gamedata.txt in Resources folder (..\Resources)

--- Day 024/scoreboard.py
+++ Day 024/scoreboard.py
@@ -2,13 +2,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
-
+        
 class Scoreboard(Turtle):
 
+
+        
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("..\Resources\snake_gamedata.txt", "r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -23,12 +26,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("..\Resources\snake_gamedata.txt", "w") as file:
+                file.write(str(self.high_score))
+                
         self.score = 0
         self.update_scoreboard()
-        
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
