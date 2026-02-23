ed22612656fdb4308bf7138fd9399a1c74aedbd8
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-22 20:26:19 -0700

    Project complete!

--- Day 024/Mail Merge Project Start/main.py
+++ Day 024/Mail Merge Project Start/main.py
@@ -6,3 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        
+        
+with open("./Input/Names/invited_names.txt", "r") as file:
+    invited_names = [name.strip() for name in file.readlines()]
+    
+with open("./Input/Letters/starting_letter.txt", "r") as file:
+    template = file.read()
+    
+for name in invited_names:
+    personalized = template.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/{name}.txt", "w") as file:
+        file.write(personalized)
