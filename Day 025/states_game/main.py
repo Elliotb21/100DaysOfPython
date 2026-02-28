122dd3e0cb1782d8bc47cb449076b053a992bf92
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-02-27 20:52:55 -0700

    boilerplate

---
+++ Day 025/states_game/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("United States Guessing Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+screen.exitonclick()
