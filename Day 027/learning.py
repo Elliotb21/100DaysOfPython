63d9c22ca4853e41c9f102a7c117bfd20f889702
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-03-29 19:38:17 -0600

    Another way using a loop.

--- Day 027/learning.py
+++ Day 027/learning.py
@@ -1,6 +1,10 @@
 def add(*args):
     return sum(args)
-    
-    
 print(add(2,2,3))
-    
+
+def subtract(*args):
+    sum = args[0]
+    for item in range(args[1]):
+        sum -= item
+    return sum
+print(subtract(5,3))
