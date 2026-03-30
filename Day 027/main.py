6d175a5ac018dcf2dd6bc1d71df4956a67447275
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-03-29 19:38:19 -0600

    Another method using a loop

--- Day 027/main.py
+++ Day 027/main.py
@@ -1,6 +1,12 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("TITLE BAR")
+window.minsize(width=500, height=300)
 
+
+#Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 14, "underline"))
+my_label.pack()
 
 window.mainloop()
