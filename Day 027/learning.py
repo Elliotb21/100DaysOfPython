e204896498201fb057ae18701411ba87492a3df5
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-03-29 20:01:51 -0600

    **kwargs use in a class

--- Day 027/learning.py
+++ Day 027/learning.py
@@ -2,9 +2,29 @@
 #     return sum(args)
 # print(add(2,2,3))
 
-def subtract(*args):
-    difference = args[0]
-    for item in args[1:]:
-        difference -= item
-    return difference
-print(subtract(1,2,3,4,5))
+# def subtract(*args):
+#     difference = args[0]
+#     for item in args[1:]:
+#         difference -= item
+#     return difference
+# print(subtract(1,2,3,4,5))
+
+
+# def calculate(num, **kwargs):
+    # for key, value in kwargs.items():
+    #     print(key, value)
+    # print(type(kwargs))    
+    # print(kwargs)
+    # print(kwargs["add"]) num + '5' is 10
+    # num *= kwargs["multiply"] num * '2' is 20
+    # return num
+    
+# print(calculate(5, add=5, multiply=2))
+
+class Car():
+    def __init__(self, **kwargs):
+        self.make = kwargs["make"]
+        self.model = kwargs["model"]
+        
+my_car = Car(make="Honda", model="Civic")
+print(my_car.make)
