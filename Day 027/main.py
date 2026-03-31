3eb6be086ddc25c2da371d8ff7c1168eff02b094
100DaysOfPython <213225573+Elliotb21@users.noreply.github.com>
2026-03-30 19:59:45 -0600

    Built a few widgets (Label, Button, Entry)

--- Day 027/main.py
+++ Day 027/main.py
@@ -1,12 +1,27 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("TITLE BAR")
 window.minsize(width=500, height=300)
 
 
 #Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 14, "underline"))
+my_label = Label(text="I am a label", font=("Arial", 14, "underline"))
 my_label.pack()
 
+#Button
+def button_click():
+    # my_label.config(text="Button was clicked!")
+    my_label.config(text=input.get())
+
+        
+my_button = Button(text="Click here!", command=button_click)
+my_button.pack(side="bottom")
+
+#Entry
+input = Entry()
+input.pack()
+
+    
+    
 window.mainloop()
